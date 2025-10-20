7884d13fdfb0e21ed74c2d51f026a56604fdd6ca
Mahnaz-karimi <72239384+Mahnaz-karimi@users.noreply.github.com>
2025-10-20 21:08:05 +0200

    getting location async & await - 141

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,13 +7,26 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  final LocationSettings locationSettings = LocationSettings(
+    accuracy: LocationAccuracy.low,
+    // distanceFilter: 100,
+  );
+  void getLocation() async {
+    // async help to get these time consuming tasks to happen in the background instead of happening in the foreground
+    Position position =
+        await Geolocator.getCurrentPosition(locationSettings: locationSettings);
+
+    LocationPermission permission = await Geolocator.requestPermission();
+    print(position);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
+        child: ElevatedButton(
           onPressed: () {
-            //Get the current location
+            getLocation(); //Get the current location
           },
           child: Text('Get Location'),
         ),
